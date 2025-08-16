5074fddaacc794efe3da9f3f188e8648dbb9cfbf
Adebola <adebolaajewole@gmail.com>
2025-08-16 08:51:08 +0100

    update done on edit and delete expense

--- expenses/views.py
+++ expenses/views.py
@@ -45,3 +45,45 @@ def add_expense(request):
         messages.success(request, 'Expense saved!')
         
     return redirect('expenses')
+
+def expense_edit(request, id):
+    expense = Expenses.objects.get(pk=id)
+    categories = Category.objects.all()
+    context = {
+        'expense': expense,
+        'values': expense,
+        'categories': categories,
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/edit_expense.html', context)
+    
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Enter amount')
+            return render(request, 'expenses/edit_expense.html', context)
+        
+        description = request.POST['description']
+        if not description:
+            messages.error(request, 'Description required')
+            return render(request, 'expenses/edit_expense.html', context)
+        
+        category = request.POST['category']
+        date = request.POST['expense_date']
+
+        expense.owner=request.user 
+        expense.amount=amount
+        expense.description=description
+        expense.category=category
+        expense.date=date
+        expense.save()
+
+        messages.success(request, 'Expense Updated!')
+        return redirect('expenses')
+    
+def expense_delete(request, id):
+    expense = Expenses.objects.get(pk=id)
+    expense.delete()
+    messages.success(request, 'Expense deleted successfully!')
+    return redirect('expenses')
