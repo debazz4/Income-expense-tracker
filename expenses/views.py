c2779c191be108411ee42156d66e2c3ea9631a10
Adebola <adebolaajewole@gmail.com>
2025-08-18 08:20:49 +0100

    Pagination added

--- expenses/views.py
+++ expenses/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Expenses, Category
 from django.contrib import messages
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -10,8 +10,12 @@ from django.contrib import messages
 @login_required(login_url='/auth/login')
 def index(request):
     expenses = Expenses.objects.filter(owner=request.user)
+    paginator = Paginator(expenses, 2)
+    page_number = request.GET.get('page')
+    page_obj =  Paginator.get_page(paginator, page_number)
     context = {
         'expenses': expenses,
+        'page_obj': page_obj
     }
     return render(request, 'expenses/index.html', context)
 
