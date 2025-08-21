2ceb5b60b24b3c9bc3ec78a7ad8cb229da17196f
Adebola <adebolaajewole@gmail.com>
2025-08-21 11:49:40 +0100

    Ajax search implemented

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
     path('', views.index, name='expenses'),
+    path('search-expenses', csrf_exempt(views.search_expenses), name='search-expenses'),
     path('add-expense', views.add_expense, name='add-expenses'),
     path('expense-edit/<int:id>', views.expense_edit, name='expense-edit'),
     path('expense-delete/<int:id>', views.expense_delete, name='expense-delete')
