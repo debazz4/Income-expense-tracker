0379be1b80619dde31e02e0ff6f246953db20650
Adebola <adebolaajewole@gmail.com>
2025-09-05 00:28:30 +0100

    admin dashboard restyled

--- expenses/admin.py
+++ expenses/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import Expenses, Category
 # Register your models here.
 
-admin.site.register(Expenses)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'description', 'category', 'date', 'owner')
+    list_filter = ('category', 'date', 'owner')
+    search_fields = ('description', 'category')
+    ordering = ('-date',)
+
+    list_per_page = 2
+
+class CategoryAdmin(admin.ModelAdmin):    
+    list_display = ('name', 'owner')
+    search_fields = ('name', 'owner__username')
+    ordering = ('name',)
+admin.site.register(Expenses, ExpenseAdmin)
 admin.site.register(Category)
