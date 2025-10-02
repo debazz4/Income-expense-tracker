967528b034f2269a62c43a78e52ab81706528396
Adebola <adebolaajewole@gmail.com>
2025-10-02 19:35:14 +0100

    can export to pdf, csv and excel file

--- expenses/views.py
+++ expenses/views.py
@@ -9,7 +9,10 @@ from django.http import JsonResponse, HttpResponse
 import datetime
 import csv
 import xlwt
-
+from django.template.loader import render_to_string
+from weasyprint import HTML, CSS
+import tempfile
+from django.db.models import Sum
 # Create your views here.
 
 def search_expenses(request):
@@ -175,4 +178,22 @@ def export_excel(request):
             ws.write(row_num, col_num, str(row[col_num]), font_style)
     wb.save(response)
 
+    return response
+
+def export_pdf(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; attachment; filename="Expenses-' + \
+        str(datetime.datetime.now()) + '.pdf'
+    
+    response['Content-Transfer-Encoding'] = 'binary'
+    expenses = Expenses.objects.filter(owner=request.user)
+    total = expenses.aggregate(Sum('amount'))
+    html_string = render_to_string('expenses/pdf-output.html', {'expenses': expenses, 'total': total})
+    html = HTML(string=html_string)
+    result = html.write_pdf()
+    with tempfile.NamedTemporaryFile(delete=True) as output:
+        output.write(result)
+        output.flush()
+        output = open(output.name, 'rb')
+        response.write(output.read())
     return response
